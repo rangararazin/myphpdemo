797d3d92c1f8137833bd68991775faa47f08c62e
rangararazin <rangararazin@outlook.com>
2023-04-05 15:18:39 +0100

    started note taking app project - ep23

--- controllers/about.php
+++ controllers/about.php
@@ -1,8 +1,4 @@
 <?php
 
-
-
 $heading = 'About Us';
 require "views/about.view.php";
-
-?>
--- index.php
+++ index.php
@@ -3,23 +3,4 @@
 
 require 'function.php';
 require 'Database.php';
-
-// require 'router.php';
-
-$config  = require('config.php');
-
-$db = new Database($config['database']);
-
-$id = $_GET['id'];
-$query = "select * from posts where id= :id ;";
-
-$posts = $db->query($query, [':id' => $id])->fetch();
-
-
-
-dd($posts);
-
-
-// foreach ($posts as $post) {
-//     echo "<li>" . $post['title'] . "</li>";
-// }
+require 'router.php';
